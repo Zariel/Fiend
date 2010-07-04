393cd12e148914bc124bb7dfb7e14290a30e9428
Chris Bannister <c.bannister@gmail.com>
2010-07-04 01:14:54 +0100

    same old roster, new name

--- ZeeRoster-1.0.lua
+++ ZeeRoster-1.0.lua
@@ -1,4 +1,15 @@
--- Rosters ;<
+--[[
+Copyright (c) 2009, 2010 Chris Bannister
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+    * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+    * Neither the name of the <ORGANIZATION> nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+]]
 
 local lib = LibStub and LibStub:NewLibrary("ZeeRoster-1.0", 1)
 
@@ -11,6 +22,8 @@ local UnitInRaid = UnitInRaid
 local UnitGUID = UnitGUID
 
 local playerGUID
+
+local pary_size = 1
 
 -- owner guid -> pet guid
 local revPets = {}
@@ -51,6 +64,8 @@ local UpdateRoster = function(base, num)
 		unit = base .. i
 
 		if(UnitExists(unit)) then
+			party_size = i
+
 			guid = UnitGUID(unit)
 
 			if guid == playerGUID then
@@ -127,6 +142,10 @@ f:RegisterEvent("UNIT_PET")
 f:RegisterEvent("ZONE_CHANGED_NEW_AREA")
 f.ZONE_CHANGED_NEW_AREA = f.PARTY_MEMBERS_CHANGED
 
+function lib:AddPet(guid, parent)
+	pets[guid] = parent
+	revPets[parent] = guid
+end
 
 function lib:IsPet(guid)
 	return pets[guid]
@@ -138,4 +157,12 @@ end
 
 function lib:IterateUnitRoster()
 	return next, units, nil
+end
+
+function lib:InRaid()
+	return party_size > 5
+end
+
+function lib:InRaid()
+	return party_size > 1 and party_size < 6
 end
